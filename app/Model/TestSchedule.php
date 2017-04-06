10aa01731d4d8e03abb4d536f7ef6ecb4b60366b
Nick Robillard <nick@ewdd.com>
2017-04-06 08:46:24 -0500

    testing app

---
+++ app/Model/TestSchedule.php
@@ -0,0 +1,57 @@
+<?php
+App::uses('AppModel', 'Model');
+/**
+ * Article Model
+ *
+ * @property ArticlePivotDetail $ArticlePivotDetail
+ */
+class TestSchedule extends AppModel {
+
+/**
+ * Display field
+ *
+ * @var string
+ */
+	public $displayField = 'name';
+
+    public $actsAs = array('Containable');
+
+	//The Associations below have been created with all possible keys, those that are not needed can be removed
+
+/**
+ * hasMany associations
+ *
+ * @var array
+ */
+	public $hasMany = array(
+		'AssignedTest',
+        'BlindTest'
+	);
+    
+    public $belongsTo = array( 
+        'Test',
+    );
+    
+    public function getAssignedTestingInfo($id=null, $blindTestId=null) {
+        $data = $this->find('first', array(
+            'conditions' => array(
+                'TestSchedule.link_num' => $id
+            ),
+            'contain'=>array(
+                'BlindTest'=>array(
+                    'conditions' =>array(
+                        'BlindTest.id' =>$blindTestId
+                    )
+                ),
+                'Test'=>array(
+                    'fields'=>array('Test.lft', 'Test.rght')
+                )
+            )
+        ));
+        
+        return $data;
+    }
+    
+    
+
+}
